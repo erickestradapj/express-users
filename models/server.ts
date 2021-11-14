9abb7b70ffc7d351607debd7fd0b0e1086a893a3
Erick Estrada <erickestradapj@gmail.com>
2021-11-13 21:19:43 -0500

    routes

--- models/server.ts
+++ models/server.ts
@@ -1,17 +1,31 @@
 import express, { Application } from 'express';
+import { Path } from '../interfaces/path.interface';
+
+import userRoutes from '../routes/user.route';
 
 export class Server {
    private app: Application;
    private port: string;
+   private paths: Path;
 
    constructor() {
       this.app = express();
-      this.port = process.env.PORT || '3000';
+      this.port = process.env.PORT || '8000';
+      this.paths = {
+         users: '/api/users',
+      };
+
+      // TODO: Routes of my application
+      this.routes();
    }
 
    public listen() {
       this.app.listen(this.port, () => {
          console.log(`App listening at http://localhost:${this.port}`);
       });
+   }
+
+   public routes() {
+      this.app.use(this.paths.users, userRoutes);
    }
 }
