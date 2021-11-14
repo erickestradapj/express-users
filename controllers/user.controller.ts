84f1105f730ea40a68aed37a8b23375d7ba4b514
Erick Estrada <erickestradapj@gmail.com>
2021-11-14 17:57:35 -0500

    controller finished

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,22 +1,54 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
 export const getUsers = async (req: Request, res: Response) => {
-   res.json({
-      msg: 'getUsers',
-   });
+   const users = await User.findAll();
+
+   res.json({ users });
 };
 
 export const getUser = async (req: Request, res: Response) => {
    const { id } = req.params;
 
+   const user = await User.findByPk(id);
+
+   if (!user) {
+      res.status(404).json({
+         msg: `Does not exist user with ID ${id}`,
+      });
+   }
+
    res.json({
-      msg: 'getUser',
-      id,
+      user,
    });
 };
 
 export const postUser = async (req: Request, res: Response) => {
    const { body } = req;
+
+   try {
+      const existEmail = await User.findOne({
+         where: {
+            email: body.email,
+         },
+      });
+
+      if (existEmail) {
+         return res.status(400).json({
+            msg: `Already exist user with email ${body.email}`,
+         });
+      }
+
+      const user = User.build(body);
+      await user.save();
+
+      res.json(user);
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         msg: 'Error - POST',
+      });
+   }
 
    res.json({
       msg: 'postUser',
@@ -28,6 +60,24 @@ export const putUser = async (req: Request, res: Response) => {
    const { id } = req.params;
    const { body } = req;
 
+   try {
+      const user = await User.findByPk(id);
+      if (!user) {
+         return res.status(404).json({
+            msg: `Does not exist user with id ${id} `,
+         });
+      }
+
+      await user.update(body);
+
+      res.json({ user });
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         msg: 'Error - POST',
+      });
+   }
+
    res.json({
       msg: 'putUser',
       id,
@@ -38,8 +88,16 @@ export const putUser = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
    const { id } = req.params;
 
+   const user = await User.findByPk(id);
+   if (!user) {
+      return res.status(404).json({
+         msg: `Does not exist user with id ${id} `,
+      });
+   }
+
+   await user.update({ status: false });
+
    res.json({
-      msg: 'deleteUser',
-      id,
+      user,
    });
 };
